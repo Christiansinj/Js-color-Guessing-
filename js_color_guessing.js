b15e5ca6b82ef32f36b1c9cd1e43bc9ee716968c
ChristianS <sinvry.christian@gmail.com>
2017-06-14 14:00:38 +0200

    Pop up + condition

--- js_color_guessing.js
+++ js_color_guessing.js
@@ -1,7 +1,13 @@
 var tab=["red","blue","yellow"]
 
 function Buen(){	
-document.getElementById("CR").style.backgroundColor = tab[Math.floor(Math.random()*3)]
+if (document.getElementById("CR").style.backgroundColor == tab[Math.floor(Math.random()*3)]){
+alert ("You win !")	
+}
+else {
+	alert ("You Loose !")
+}
+document.getElementById('CR').style.backgroundColor = tab[Math.floor(Math.random()*tab.length)]
 }
 
 document.getElementById("bouton").addEventListener("click",Buen)
